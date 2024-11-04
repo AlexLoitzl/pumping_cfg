e2a0274616041dffa34b5a69bbcfd6b72724a24e
AlexLoitzl <alexander.loitzl@gmail.com>
2024-11-04 15:14:53 +0100

    Rename and refactor

--- PumpingCfg/EpsilonElimination.lean
+++ PumpingCfg/EpsilonElimination.lean
@@ -386,24 +386,19 @@ lemma compute_nullables_iff (v : g.NT) :
 -- ************************************* Epsilon Elimination ************************************* --
 -- *********************************************************************************************** --
 
-def remove_nullables' (nullable : Finset g.NT) (output : List (Symbol T g.NT)) : List (List (Symbol T g.NT)) :=
+def remove_nullable (nullable : Finset g.NT) (output : List (Symbol T g.NT)) : List (List (Symbol T g.NT)) :=
   match output with
   | [] => []
   | x :: xs => match x with
-               | Symbol.nonterminal n => (if n ∈ nullable then remove_nullables' nullable xs else [])
-                                         ++ List.map (fun y => x :: y) (remove_nullables' nullable xs)
-               | Symbol.terminal _ => List.map (fun y => x :: y) (remove_nullables' nullable xs)
-
-def remove_nullable (nullable : Finset g.NT) (s: (Symbol T g.NT)) (acc : List (List (Symbol T g.NT))) :=
-  match s with
-  | Symbol.nonterminal n => (if n ∈ nullable then acc else []) ++ acc.map (fun x => s :: x)
-  | Symbol.terminal _ => acc.map (fun x => s :: x)
+               | Symbol.nonterminal n => (if n ∈ nullable then remove_nullable nullable xs else [])
+                                         ++ List.map (fun y => x :: y) (remove_nullable nullable xs)
+               | Symbol.terminal _ => List.map (fun y => x :: y) (remove_nullable nullable xs)
 
 def remove_nullable_rule (nullable : Finset g.NT) (r: ContextFreeRule T g.NT) : (List (ContextFreeRule T g.NT)) :=
   let fltrmap : List (Symbol T g.NT) → Option (ContextFreeRule T g.NT)
     | [] => Option.none
     | h :: t => ContextFreeRule.mk r.input (h :: t)
-  (r.output.foldr (remove_nullable nullable) [[]]).filterMap fltrmap
+  (remove_nullable nullable r.output).filterMap fltrmap
 
 def remove_nullables (nullable : Finset g.NT) : List (ContextFreeRule T g.NT) :=
   (g.rules.map (remove_nullable_rule nullable)).join
@@ -504,17 +499,18 @@ lemma nullable_related_derivable {xs ys : List (Symbol T g.NT)} (h: NullableRela
     exact ih
     exact hn
 
-lemma l4 {o o': List (Symbol T g.NT)} (nullable : Finset g.NT) (p : ∀ x ∈ nullable, NullableNonTerminal x)
-  (hin : o ∈ (remove_nullables' nullable o')) : NullableRelated o o' := by
+lemma remove_nullable_related {o o': List (Symbol T g.NT)} (nullable : Finset g.NT)
+  (p : ∀ x ∈ nullable, NullableNonTerminal x) (hin : o ∈ (remove_nullable nullable o')) :
+  NullableRelated o o' := by
   revert o
   induction o' with
   | nil =>
     intro o hin
-    unfold remove_nullables' at hin
+    unfold remove_nullable at hin
     contradiction
   | cons hd tl ih =>
     intro o hin
-    unfold remove_nullables' at hin
+    unfold remove_nullable at hin
     cases hd with
     | nonterminal nt =>
       simp at hin
@@ -524,28 +520,30 @@ lemma l4 {o o': List (Symbol T g.NT)} (nullable : Finset g.NT) (p : ∀ x ∈ nu
         exact ih h
         exact p _ h'
       · obtain ⟨o1, hoin, ho⟩ := h
-        rw[←ho]
+        rw [←ho]
         constructor
         exact ih hoin
     | terminal t =>
       simp at hin
       obtain ⟨o1, hoin, ho⟩ := hin
-      rw[←ho]
+      rw [←ho]
       constructor
       exact ih hoin
 
-lemma l5 {r': ContextFreeRule T g.NT} {r : ContextFreeRule T (@eliminate_empty T g).NT}
-  {nullable : Finset g.NT} {h : r ∈ remove_nullable_rule nullable r'} :
-  r.input = r'.input ∧ @NullableRelated _ g r.output r'.output := by sorry
---   unfold remove_nullable_rule at h
---   rw [List.mem_filterMap] at h
---   obtain ⟨o, ho1, ho2⟩ := h
---   cases o <;> simp at ho2
---   rw [←ho2]
---   constructor
---   rfl
---   apply l4
---   exact ho1
+lemma remove_nullable_rule_related {r': ContextFreeRule T g.NT}
+  {r : ContextFreeRule T (@eliminate_empty T g).NT} {h : r ∈ remove_nullable_rule compute_nullables r'} :
+  r.input = r'.input ∧ @NullableRelated _ g r.output r'.output := by
+  unfold remove_nullable_rule at h
+  rw [List.mem_filterMap] at h
+  obtain ⟨o, ho1, ho2⟩ := h
+  cases o <;> simp at ho2
+  rw [←ho2]
+  constructor
+  rfl
+  apply remove_nullable_related
+  intro
+  apply (compute_nullables_iff _).1
+  exact ho1
 
 lemma eliminate_empty_rules (r : ContextFreeRule T (@eliminate_empty T g).NT) {h : r ∈ (@eliminate_empty T g).rules} :
   ∃ r' ∈ g.rules, r.input = r'.input ∧ @NullableRelated _ g r.output r'.output := by
@@ -555,7 +553,7 @@ lemma eliminate_empty_rules (r : ContextFreeRule T (@eliminate_empty T g).NT) {h
   use r'
   constructor
   exact hrin'
-  apply l5
+  apply remove_nullable_rule_related
   exact hr'
 
 lemma eliminate_empty_step_derives {v w : List (Symbol T g.NT)} (h : (@eliminate_empty T g).Produces v w) :
@@ -584,6 +582,7 @@ lemma eliminate_empty_implies {v w : List (Symbol T g.NT)} {n : ℕ}
     exact eliminate_empty_implies hd
 
 theorem eliminate_empty_correct :
-  g.language = (@eliminate_empty T g).language \ {[]} := by sorry
+  g.language = (@eliminate_empty T g).language \ {[]} := by
+  sorry
 
 end ContextFreeGrammar
