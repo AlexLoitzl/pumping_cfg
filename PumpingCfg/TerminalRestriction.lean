dd8fa852ed0e1ca458be6953ee78d95d16dd1e7c
AlexLoitzl <alexander.loitzl@gmail.com>
2024-11-25 11:04:24 +0100

    Prove oif of algorithm and final theorem direction

--- PumpingCfg/TerminalRestriction.lean
+++ PumpingCfg/TerminalRestriction.lean
@@ -104,6 +104,35 @@ lemma lift_terminals {w : List T} :
       sorry
     · sorry
 
+lemma unlift_lift_eq {w : List (Symbol T NT)} : unlift_string (lift_string w) = w := by
+  induction w with
+  | nil => rfl
+  | cons hd tl ih =>
+    unfold lift_string unlift_string at *
+    simp
+    constructor
+    · unfold unlift_symbol lift_symbol
+      cases hd <;> rfl
+    · simp at ih
+      exact ih
+
+lemma unlift_string_terminals {w : List T} :
+  unlift_string (List.map (@Symbol.terminal T (NT ⊕ T)) w) = (List.map (@Symbol.terminal T NT) w) := by
+  induction w with
+  | nil => rfl
+  | cons hd tl ih =>
+    unfold unlift_string at ih ⊢
+    simp at ih ⊢
+    constructor
+    · unfold unlift_symbol
+      rfl
+    · exact ih
+
+lemma unlift_symbol_nonterminal {nt : NT} :
+  unlift_symbol (@Symbol.nonterminal T (NT ⊕ T) (Sum.inl nt)) = Symbol.nonterminal nt := by
+  unfold unlift_symbol
+  rfl
+
 end TerminalRelated
 
 -- *********************************************************************************************** --
@@ -120,7 +149,7 @@ def new_terminal_rules {NT : Type} (r : ContextFreeRule T NT) : List (ContextFre
   r.output.filterMap terminal_rule
 
 def restrict_terminal_rule {NT : Type} (r : ContextFreeRule T NT) : List (ContextFreeRule T (NT ⊕ T)) :=
-  (ContextFreeRule.mk (Sum.inl r.input) (r.output.map lift_symbol)) :: new_terminal_rules r
+  (ContextFreeRule.mk (Sum.inl r.input) (lift_string r.output)) :: new_terminal_rules r
 
 def restrict_terminal_rules {NT : Type} (rs : List (ContextFreeRule T NT)) : List (ContextFreeRule T (NT ⊕ T)) :=
   (rs.map restrict_terminal_rule).join
@@ -133,16 +162,64 @@ end RestrictTerminals
 -- *********************************************************************** --
 -- Only If direction of the main correctness theorem of restrict_terminals --
 -- *********************************************************************** --
+
 section CorrectnessProof
 
 variable {g : ContextFreeGrammar.{0,0} T}
 
-lemma restrict_terminals_rule_right {t : T} {r' : ContextFreeRule T (g.NT ⊕ T)}
-  (h : r' ∈ restrict_terminal_rules g.rules) (h' : r'.input = Sum.inr t) : r'.output = [Symbol.terminal t] := by sorry
+lemma restrict_terminal_rule_right {t : T} {r : ContextFreeRule T g.NT}
+  {r' : ContextFreeRule T (g.NT ⊕ T)} (h : r' ∈ restrict_terminal_rule r) (h' : r'.input = Sum.inr t) :
+  r'.output = [Symbol.terminal t] := by
+  unfold restrict_terminal_rule at h
+  simp at h
+  cases h <;> rename_i h
+  · rw [h] at h'
+    simp at h'
+  · unfold new_terminal_rules at h
+    simp at h
+    obtain ⟨s, hsin, h⟩ := h
+    cases s <;> simp at h
+    rw [←h] at h' ⊢
+    simp at h' ⊢
+    exact h'
 
-lemma restrict_terminals_rule_left {nt : g.NT} {r' : ContextFreeRule T (g.NT ⊕ T)}
+lemma restrict_terminals_rule_right {t : T} {r' : ContextFreeRule T (g.NT ⊕ T)}
+  (h : r' ∈ restrict_terminal_rules g.rules) (h' : r'.input = Sum.inr t) : r'.output = [Symbol.terminal t] := by
+  unfold restrict_terminal_rules at h
+  simp at h
+  obtain ⟨r, _, h⟩ := h
+  exact restrict_terminal_rule_right h h'
+
+
+lemma restrict_terminal_rule_left {nt : g.NT} {r : ContextFreeRule T g.NT}
+  {r' : ContextFreeRule T (g.NT ⊕ T)} (h : r' ∈ restrict_terminal_rule r) (h' : r'.input = Sum.inl nt) :
+  r.input = nt ∧ r.output = unlift_string r'.output := by
+  unfold restrict_terminal_rule at h
+  simp at h
+  cases h <;> rename_i h
+  · rw [h] at h' ⊢
+    simp at h' ⊢
+    constructor
+    · exact h'
+    · rw [unlift_lift_eq]
+  · unfold new_terminal_rules at h
+    simp at h
+    obtain ⟨r'', hrin, h⟩ := h
+    cases r'' <;> simp at h
+    rw [← h] at h'
+    simp at h'
+
+lemma restrict_terminals_rules_left {nt : g.NT} {r' : ContextFreeRule T (g.NT ⊕ T)}
   (h : r' ∈ restrict_terminal_rules g.rules) (h' : r'.input = Sum.inl nt) :
-  ∃ r ∈ g.rules, r.input = nt ∧ r.output = unlift_string r'.output := by sorry
+  ∃ r ∈ g.rules, r.input = nt ∧ r.output = unlift_string r'.output := by
+  unfold restrict_terminal_rules at h
+  simp at h
+  obtain ⟨r, hrin, hr⟩ := h
+  apply restrict_terminal_rule_left at hr
+  use r
+  constructor
+  exact hrin
+  exact hr h'
 
 lemma restrict_terminals_produces_derives {u' v' : List (Symbol T (g.NT ⊕ T))}
   (h : (restrict_terminals g).Produces u' v') : g.Derives (unlift_string u') (unlift_string v') := by
@@ -150,7 +227,7 @@ lemma restrict_terminals_produces_derives {u' v' : List (Symbol T (g.NT ⊕ T))}
   obtain ⟨p, q, hu', hv'⟩ := hr'.exists_parts
   cases h : r'.input with
   | inl nt =>
-    obtain ⟨r, hrin, hri, hro⟩ := restrict_terminals_rule_left hrin' h
+    obtain ⟨r, hrin, hri, hro⟩ := restrict_terminals_rules_left hrin' h
     rw [hu', hv', h]
     unfold unlift_string at hro ⊢
     repeat rw [List.map_append]
@@ -227,7 +304,15 @@ theorem eliminate_empty_correct :
     apply Derives.trans h3
 
     sorry
-  · sorry
+  · intro w h
+    simp at h ⊢
+    unfold Generates at h ⊢
+    apply restrict_terminals_implies' at h
+    rw [unlift_string_terminals] at h
+    unfold restrict_terminals unlift_string at h
+    simp at h
+    rw [unlift_symbol_nonterminal] at h
+    exact h
 
 end CorrectnessProof
 
