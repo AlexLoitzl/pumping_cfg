3582044c6406e08f2d9954cb9d04c5d96de4d264
AlexLoitzl <alexander.loitzl@gmail.com>
2024-11-26 17:21:31 +0100

    Define final CNF translation

--- PumpingCfg/LengthRestriction.lean
+++ PumpingCfg/LengthRestriction.lean
@@ -22,15 +22,32 @@ variable {g : ContextFreeGrammar T}
 abbrev NT'2 : Type := g.NT ⊕ Σ r : {x | x ∈ g.rules}, Fin (r.1.output.length - 2)
 abbrev NT' : Type := g.NT ⊕ Σ r : ContextFreeRule T g.NT, Fin (r.output.length - 2)
 
-def restrict_length_rule (r : ContextFreeRule T g.NT) : List (CNFRule T (g.NT')) := []
+def compute_rules_rec (r : ContextFreeRule T g.NT) (i : Fin (r.output.length - 2)) : List (CNFRule T (g.NT')) :=
+  match i with
+  | ⟨0, p⟩ => match r.output.get ⟨r.output.length - 2, by omega⟩, r.output.get ⟨r.output.length - 1, by omega⟩ with
+             | Symbol.nonterminal nt1, Symbol.nonterminal nt2 => [(CNFRule.node (Sum.inr ⟨r, ⟨0, p⟩⟩) (Sum.inl nt1) (Sum.inl nt2))]
+             | _, _ => []
+  | ⟨n + 1, p⟩ => match r.output.get ⟨r.output.length - 2 - i.val, by omega⟩ with
+                 | Symbol.nonterminal nt => (CNFRule.node (Sum.inr ⟨r, ⟨i.val,by omega⟩⟩) (Sum.inl nt) (Sum.inr ⟨r, ⟨n,by omega⟩⟩))
+                   :: compute_rules_rec r ⟨n, by omega⟩
+                 | _ => []
+
+def compute_rules (r : ContextFreeRule T g.NT) : List (CNFRule T (g.NT')) :=
+  match h : r.output with
+  | [Symbol.nonterminal nt1, Symbol.nonterminal nt2] => [CNFRule.node (Sum.inl r.input) (Sum.inl nt1) (Sum.inl nt2)]
+  | [Symbol.terminal t] => [CNFRule.leaf (Sum.inl r.input) t]
+  | Symbol.nonterminal nt :: _ :: _ :: _ => (CNFRule.node (Sum.inl r.input) (Sum.inl nt) (Sum.inr ⟨r, ⟨r.output.length - 3, by rw [h]; simp⟩⟩))
+                                            :: compute_rules_rec r ⟨r.output.length - 3, by rw [h]; simp⟩
+  | _ => []
 
 def restrict_length_rules (rs : List (ContextFreeRule T g.NT)) : List (CNFRule T (g.NT')) :=
-  (rs.map restrict_length_rule).join
+  (rs.map compute_rules).join
 
 end RestrictLength
 
 def restrict_length (g : ContextFreeGrammar.{0,0} T) : (CNF T) :=
   CNF.mk g.NT' (Sum.inl g.initial) (restrict_length_rules g.rules)
+
 -- ******************************************************************** --
 -- Only If direction of the main correctness theorem of restrict_length --
 -- ******************************************************************** --
