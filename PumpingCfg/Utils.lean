70a1b9fc0988e2b4c6eb2e83a940c388c60cc12c
madvorak <martin.dvorak@matfyz.cz>
2024-12-18 10:57:19 +0100

    things can be implicit

--- PumpingCfg/Utils.lean
+++ PumpingCfg/Utils.lean
@@ -7,21 +7,23 @@ def nTimes (l : List α) (n : ℕ) : List α :=
 
 infixl:69 " ^+^ " => nTimes
 
-lemma nTimes_succ_l {l : List α} {n : ℕ} : l^+^n.succ = l ++ l^+^n := by
+variable {l : List α} {n : ℕ}
+
+lemma nTimes_succ_l : l^+^n.succ = l ++ l^+^n := by
   simp [nTimes, List.replicate_succ]
 
-lemma nTimes_succ_r {l : List α} {n : ℕ} : l^+^n.succ = l^+^n ++ l := by
+lemma nTimes_succ_r : l^+^n.succ = l^+^n ++ l := by
   simp [nTimes, List.replicate_succ']
 
-lemma nTimes_map {β : Type _} {l : List α} {f : α → β} {n : ℕ} : (l.map f)^+^n = (l^+^n).map f := by
+lemma nTimes_map {β : Type _} {f : α → β} : (l.map f)^+^n = (l^+^n).map f := by
   simp [nTimes]
 
-lemma nTimes_add {l : List α} {m n : ℕ} : l ^+^ (m + n) = l ^+^ m ++ l ^+^ n := by
+lemma nTimes_add {m : ℕ} : l ^+^ (m + n) = l ^+^ m ++ l ^+^ n := by
   induction n with
   | zero => exact (l ^+^ m).append_nil.symm
-  | succ k ih => rw [Nat.add_succ, nTimes_succ_r, nTimes_succ_r, ih, List.append_assoc]
+  | succ _ ih => rw [Nat.add_succ, nTimes_succ_r, nTimes_succ_r, ih, List.append_assoc]
 
-lemma nTimes_mul {l : List α} {m n : ℕ} : l ^+^ m * n = l ^+^ m ^+^ n := by
+lemma nTimes_mul {m : ℕ} : l ^+^ m * n = l ^+^ m ^+^ n := by
   induction n with
   | zero => rfl
-  | succ k ih => rw [Nat.mul_succ, nTimes_add, ih, nTimes_succ_r]
+  | succ _ ih => rw [Nat.mul_succ, nTimes_add, ih, nTimes_succ_r]
