50933493c5956d4c97360337a9fdfdcbc699f34a
madvorak <martin.dvorak@matfyz.cz>
2024-12-18 14:52:43 +0100

    towards subtree_repeat_root_height

--- PumpingCfg/Pumping.lean
+++ PumpingCfg/Pumping.lean
@@ -39,12 +39,64 @@ lemma pumping_string {u v : List (Symbol T g.NT)} {n : g.NT}
     repeat rw [← List.append_assoc]
     rw [← nTimes_succ_r]
 
+lemma subtree_height_le {n₁ n₂ : g.NT} {p₁ : parseTree n₁} {p₂ : parseTree n₂} (hpp : p₂.IsSubtreeOf p₁) :
+    p₂.height ≤ p₁.height := by
+  induction hpp with
+  | leaf_refl hrn => rfl
+  | node_refl p₁ p₂ hrn => rfl
+  | left_sub p₁ p₂ p hrn₁ hpp₁ ih => exact Nat.le_add_right_of_le (le_sup_of_le_left ih)
+  | right_sub p₁ p₂ p hrn₁ hpp₂ ih => exact Nat.le_add_right_of_le (le_sup_of_le_right ih)
+
 variable [DecidableEq g.NT]
+
+lemma subtree_repeat_root_height_aux_aux_aux {n : g.NT} {p : parseTree n}
+    (hp : g.generators.card.succ = p.height) :
+    ∃ n' : g.NT, ∃ p' p'' : parseTree n',
+      p'.IsSubtreeOf p ∧ p''.IsSubtreeOf p' ∧ p' ≠ p'' := by
+  sorry
+
+lemma subtree_repeat_root_height_aux_aux {n : g.NT} {p : parseTree n}
+    (hgp : g.generators.card.succ = p.height) :
+    ∃ n' : g.NT, ∃ p' p'' : parseTree n',
+      p'.IsSubtreeOf p ∧ p''.IsSubtreeOf p' ∧ p'.height ≤ g.generators.card.succ ∧ p' ≠ p'' := by
+  obtain ⟨n', p', p'', hp', hp'', hp⟩ := subtree_repeat_root_height_aux_aux_aux hgp
+  exact ⟨n', p', p'', hp', hp'', hgp ▸ subtree_height_le hp', hp⟩
+
+lemma subtree_repeat_root_height_aux {n : g.NT} {p : parseTree n}
+    (hgp : g.generators.card.succ ≤ p.height) :
+    ∃ n' : g.NT, ∃ p' p'' : parseTree n',
+      p'.IsSubtreeOf p ∧ p''.IsSubtreeOf p' ∧ p'.height ≤ g.generators.card.succ ∧ p' ≠ p'' := by
+  induction p with
+  | @tree_leaf n t =>
+    exfalso
+    have hn : n ∈ g.generators := by
+      simp [generators]
+      use ChomskyNormalFormRule.leaf n t
+    simp [parseTree.height] at hgp
+    rw [hgp] at hn
+    simp at hn
+  | tree_node t₁ t₂ hnc ih₁ ih₂ =>
+    rw [Nat.le_iff_lt_or_eq] at hgp
+    cases hgp with
+    | inr hp => exact subtree_repeat_root_height_aux_aux hp
+    | inl hp =>
+      simp [parseTree.height] at hp
+      cases hp with
+      | inl hp₁ =>
+        obtain ⟨n', p', p'', hp', hp'', hpg, hpp⟩ := ih₁ hp₁
+        exact ⟨n', p', p'', parseTree.IsSubtreeOf.left_sub t₁ t₂ p' hnc hp', hp'', hpg, hpp⟩
+      | inr hp₂ =>
+        obtain ⟨n', p', p'', hp', hp'', hpg, hpp⟩ := ih₂ hp₂
+        exact ⟨n', p', p'', parseTree.IsSubtreeOf.right_sub t₁ t₂ p' hnc hp', hp'', hpg, hpp⟩
 
 lemma subtree_repeat_root_height {n : g.NT} {p : parseTree n}
     (hp : p.yield.length ≥ 2 ^ g.generators.card) :
-    ∃ (n' : g.NT) (p₁ p₂ : parseTree n'),
-      p₁.IsSubtreeOf p ∧ p₂.IsSubtreeOf p₁ ∧ p₁.height ≤ g.generators.card ∧ p₁ ≠ p₂:= by sorry
+    ∃ n' : g.NT, ∃ p' p'' : parseTree n',
+      p'.IsSubtreeOf p ∧ p''.IsSubtreeOf p' ∧ p'.height ≤ g.generators.card.succ ∧ p' ≠ p'' := by
+  apply subtree_repeat_root_height_aux
+  have hp1 := p.height_pos
+  have hp2 := hp.trans p.yield_length_le_two_pow_height
+  rw [Nat.pow_le_pow_iff_right] at hp2 <;> omega
 
 lemma cnf_pumping {w : List T} (hwg : w ∈ g.language) (hw : w.length ≥ 2 ^ g.generators.card) :
     ∃ u v x y z : List T,
